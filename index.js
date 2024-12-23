2a879bbdc16b9f94393a3e54c8e80b2fe793abb1
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-12-23 20:07:17 +0600

    get data, put data and post data done

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 // DB_USER = product_hunter
 // DB_PASS = v8N96RnHtI9m4RfV
@@ -26,10 +26,20 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
+    await client.connect();
     const productCollection = client.db('productsItem').collection('products')
+
+
     app.get('/addQueries', async(req, res)=>{
       const cursor = productCollection.find()
       const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/queries/:email/:id', async(req, res) => {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query)
       res.send(result)
     })
 
@@ -39,8 +49,54 @@ async function run() {
       const result = await productCollection.insertOne(newQueries)
       res.send(result)
     })
+
+    app.get('/queries/:email', async(req, res) =>{
+      const email = req.params.email
+      // console.log(email)
+      if(!email){
+        return res.status(400).send({ message: 'Email is required'})
+      }
+      const cursor = productCollection.find({ userEmail : email})
+      const result = await cursor.toArray()
+      console.log(result)
+      if(result.length === 0){
+        return res.status(404).send({ message: 'Unfortunately , No product found for this user.'})
+      }
+      res.send(result)
+    })
+
+    app.put('/queries/:id', async(req, res) =>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true}
+      const updateQueries = req.body 
+      const queries = {
+        $set: {
+        productName: updateQueries.productName,
+        productBrand: updateQueries.productBrand,
+        PhotoURL: updateQueries.PhotoURL,
+        queryTitle: updateQueries.queryTitle,
+        BoycottingReasonDetails: updateQueries.BoycottingReasonDetails,
+        // userEmail: updateQueries.userEmail,
+        // name: updateQueries.name,
+        // userImage: updateQueries.userImage,
+        // currentDate: updateQueries.currentDate, 
+        // recommendationCount: updateQueries.recommendationCount  
+        }
+      }
+      const result = await productCollection.updateOne(filter, queries, options)
+      res.send(result)
+    })
+
+    
+
+    app.delete('/queries/:id', async(req, res)=>{
+      const id = req.params.id 
+      const query = { _id: new ObjectId(id) }
+      const result = await productCollection.deleteOne(query)
+      res.send(result)
+    })
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
