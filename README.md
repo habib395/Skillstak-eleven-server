417ceba4627b6fa133a96dcf3dcaa201f19590bd
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-12-29 15:13:36 +0600

    readme.md file added

---
+++ README.md
@@ -0,0 +1,22 @@
+# Website Name: SkillStack
+
+# Purpose: 
+The website helps users find and share product recommendations, ask questions about products, and manage their queries, recommendations, and comments easily.
+
+
+# live URL: https://discreet-birth.surge.sh/
+
+# key features:
+--Responsive Design
+--Reusability with Components
+--Dynamic Product Rendering
+--Search Functionality
+--Customizable Grid Layout
+--Reusable Query Component
+--Dynamic Data Fetching
+
+# npm packages: 
+--Swiper
+--Axios
+--React Awesome Reveal
+--dotenv
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
 
-// DB_USER = product_hunter
-// DB_PASS = v8N96RnHtI9m4RfV
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6aryg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -26,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    await client.connect();
+    // await client.connect();
     const productCollection = client.db('productsItem').collection('products')
     const reProductCollection = client.db('productsItem').collection('reProducts')
 
@@ -45,7 +43,7 @@ async function run() {
         query = {
           productName: {
             $regex: search,
-            $options: 'i', // Case-insensitive search
+            $options: 'i', 
           },
         };
       }
@@ -59,11 +57,7 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
-    // app.get('/myRecommendation', async(req, res) =>{
-    //   const cursor = reProductCollection.find()
-    //   const result = await cursor.toArray()
-    //   res.send(result)
-    // })
+   
 
     app.get('/addRecommendation/:queryId', async(req, res) =>{
       const id = req.params.queryId 
@@ -82,14 +76,10 @@ async function run() {
       try{
         const email = req.params.email
       console.log(email)
-      // if(!email){
-      //   return res.status(400).send({ message: "Email is require."})
-      // }
+   
       const cursor = reProductCollection.find({ recommenderEmail : email})
       const result = await cursor.toArray()
-      // if(result.length === 0){
-      //   return res.status(404).send({ message: "Unfortunately, No Product found for this user."})
-      // }
+   
       res.send(result)
       }catch(error){
         console.log(error)
